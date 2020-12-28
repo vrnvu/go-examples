1a83a797a2ba7b3b2d327b344eed7844cee74cd7
Arnau Diaz <diazarnau0@gmail.com>
2020-12-28 14:42:23 +0100

    feature: entry point with a Hello function

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// Hello function returns a hardcoded value
+func Hello() string {
+	return "hello mv"
+}
+
+func main() {
+	fmt.Println(Hello())
+}
+
+//  LocalWords:  mv
